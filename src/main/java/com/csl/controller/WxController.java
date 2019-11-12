e51cd49efe9ca394a1d39bc2acc753f3c73f2103
Sihai.wang@sc.com <Sihai.wang@sc.com>
2019-11-12 20:42:16 +0800

    change properties

--- src/main/java/com/csl/controller/WxController.java
+++ src/main/java/com/csl/controller/WxController.java
@@ -30,48 +30,49 @@ import com.soecode.wxtools.util.xml.XStreamTransformer;
 @RequestMapping("/wx")
 public class WxController {
 
-    private IService iService = new WxService();
+	private IService iService = new WxService();
 
-    @GetMapping
-    public String check(String signature, String timestamp, String nonce, String echostr) {
-        if (iService.checkSignature(signature, timestamp, nonce, echostr)) {
-            return echostr;
-        }
-        return null;
-    }
+	@GetMapping
+	public String check(String signature, String timestamp, String nonce, String echostr) {
+		System.out.println("Check authentication code. ");
+		if (iService.checkSignature(signature, timestamp, nonce, echostr)) {
+			return echostr;
+		}
+		return "Access denied.";
+	}
 
-    @PostMapping
-    public void handle(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
-        PrintWriter out = response.getWriter();
+	@PostMapping
+	public void handle(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
 
-        // 创建一个路由器
-        WxMessageRouter router = new WxMessageRouter(iService);
-        try {
-            // 微信服务器推送过来的是XML格式。
-            WxXmlMessage wx = XStreamTransformer.fromXml(WxXmlMessage.class, request.getInputStream());
-            System.out.println("消息：\n " + wx.toString());
-            router.rule().msgType(WxConsts.XML_MSG_TEXT).matcher(new WhoAmIMatcher()).handler(new WhoAmIHandler()).end()
-                    .rule().msgType(WxConsts.XML_MSG_TEXT).handler(ConfigHander.getInstance()).end()
-                    .rule().event(WxConsts.EVT_CLICK).eventKey(MenuKey.HELP).handler(HelpDocHandler.getInstance()).next()
-                    .rule().eventKey(MenuKey.CHANGE_NEWS).handler(ChangeNewsHandler.getInstance()).next()
-                    .rule().eventKey(MenuKey.HOT_SONG).handler(RankHandler.getInstance()).next()
-                    .rule().eventKey(MenuKey.TOP_500).handler(RankHandler.getInstance()).next()
-                    .rule().eventKey(MenuKey.NET_HOT_SONG).handler(RankHandler.getInstance()).next()
-                    .rule().eventKey(MenuKey.HUAYU_SONG).handler(RankHandler.getInstance()).next()
-                    .rule().eventKey(MenuKey.XINAO_SONG).handler(RankHandler.getInstance()).end();
-            // 把消息传递给路由器进行处理
-            WxXmlOutMessage xmlOutMsg = router.route(wx);
-            if (xmlOutMsg != null)
-                out.print(xmlOutMsg.toXml());// 因为是明文，所以不用加密，直接返回给用户。
+		// 创建一个路由器
+		WxMessageRouter router = new WxMessageRouter(iService);
+		try {
+			// 微信服务器推送过来的是XML格式。
+			WxXmlMessage wx = XStreamTransformer.fromXml(WxXmlMessage.class, request.getInputStream());
+			System.out.println("消息：\n " + wx.toString());
+			router.rule().msgType(WxConsts.XML_MSG_TEXT).matcher(new WhoAmIMatcher()).handler(new WhoAmIHandler()).end()
+					.rule().msgType(WxConsts.XML_MSG_TEXT).handler(ConfigHander.getInstance()).end().rule()
+					.event(WxConsts.EVT_CLICK).eventKey(MenuKey.HELP).handler(HelpDocHandler.getInstance()).next()
+					.rule().eventKey(MenuKey.CHANGE_NEWS).handler(ChangeNewsHandler.getInstance()).next().rule()
+					.eventKey(MenuKey.HOT_SONG).handler(RankHandler.getInstance()).next().rule()
+					.eventKey(MenuKey.TOP_500).handler(RankHandler.getInstance()).next().rule()
+					.eventKey(MenuKey.NET_HOT_SONG).handler(RankHandler.getInstance()).next().rule()
+					.eventKey(MenuKey.HUAYU_SONG).handler(RankHandler.getInstance()).next().rule()
+					.eventKey(MenuKey.XINAO_SONG).handler(RankHandler.getInstance()).end();
+			// 把消息传递给路由器进行处理
+			WxXmlOutMessage xmlOutMsg = router.route(wx);
+			if (xmlOutMsg != null)
+				out.print(xmlOutMsg.toXml());// 因为是明文，所以不用加密，直接返回给用户。
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            out.close();
-        }
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			out.close();
+		}
 
-    }
+	}
 
 }
